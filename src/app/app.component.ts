1a96d355966064c8934fa4f82929a5dcc63c9af3
Vadym Borovok <vadym.borovok@gmail.com>
2019-08-27 09:21:41 +0300

    same, use @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -7,10 +7,14 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) signupForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
-  onSubmit(form: NgForm) {
-    console.log(form); // put object with fields "values"
+  // onSubmit(form: NgForm) {
+  //   console.log(form); // put object with fields "values"
+  // }
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
