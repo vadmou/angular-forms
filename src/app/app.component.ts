1171c3761a7408b5d78b055463d8da0067e40ba3
Vadym Borovok <vadym.borovok@gmail.com>
2019-08-28 08:44:41 +0300

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', {static: false}) signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
