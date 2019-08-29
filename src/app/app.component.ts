fdde8c8c43b7bb21877bdb12799e859cf508012b
Vadym Borovok <vadym.borovok@gmail.com>
2019-08-29 17:05:45 +0300

    reset data in form after submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,5 +46,6 @@ export class AppComponent {
         this.user.secretQuestion = this.signupForm.value.secret;
         this.user.answer = this.signupForm.value.questionAnswer;
         this.user.gender = this.signupForm.value.gender;
+        this.signupForm.reset();
     }
 }
